a121d9c64a725c7a6e79467e099c800f174331bc
Vladislav <v.butirsky@gmail.com>
2022-05-23 22:13:17 +0700

    Added roles

--- src/app/api/v1/registration.py
+++ src/app/api/v1/registration.py
@@ -1,14 +1,11 @@
 import re
 from http import HTTPStatus
 
-import werkzeug
-from flask import session, url_for
 from jsonschema import draft4_format_checker
 from psycopg2.errors import UniqueViolation
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash
 
-import config
 from db.db import db
 from db.db_models import User
 
@@ -33,7 +30,8 @@ def register(body: dict) -> tuple[str, HTTPStatus]:
         user = User(
             first_name=body['first_name'], second_name=body['second_name'],
             login=body['login'], email=body['email'],
-            password=generate_password_hash(body['password'])
+            password=generate_password_hash(body['password']),
+            role=None
         )
         db.session.add(user)
         db.session.commit()
@@ -43,74 +41,3 @@ def register(body: dict) -> tuple[str, HTTPStatus]:
             return 'User already exists', HTTPStatus.BAD_REQUEST
         else:
             return 'Unexpected error', HTTPStatus.BAD_REQUEST
-
-
-def register_oauth(provider: str) -> werkzeug.wrappers.response.Response:
-    """Функция для регистрации через OAuth Google,
-    перенаправляет в registerauthorize для авторизации через Google"""
-    google = config.oauth.create_client('google')
-    redirect_uri = url_for('/api/v1.app_api_v1_registration_registerauthorize',
-                           _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-
-def registerauthorize() -> tuple[str, HTTPStatus]:
-    """Функция для регистрации через OAuth Google"""
-    google = config.oauth.create_client('google')
-    token = google.authorize_access_token()
-    resp = google.get('userinfo', token=token)
-    user_info = resp.json()
-    user = config.oauth.google.userinfo()
-
-    try:
-        user = User(
-                first_name=user['given_name'], second_name=user['family_name'],
-                login=user['email'], email=user['email'], password=None
-        )
-        db.session.add(user)
-        db.session.commit()
-    except IntegrityError as err:
-        if isinstance(err.orig, UniqueViolation):
-            return 'User already exists', HTTPStatus.BAD_REQUEST
-
-    return 'Registered successfully', HTTPStatus.CREATED
-
-
-def login(provider: str) -> werkzeug.wrappers.response.Response:
-    """Функция для входа через OAuth Google,
-    перенаправляет в loginauthorize для авторизации через Google"""
-    google = config.oauth.create_client('google')
-    redirect_uri = url_for('/api/v1.app_api_v1_registration_loginauthorize',
-                           _external=True)
-    return google.authorize_redirect(redirect_uri)
-
-
-def loginauthorize() -> tuple[str, HTTPStatus]:
-    """Функция для входа через OAuth Google"""
-    google = config.oauth.create_client('google')
-    token = google.authorize_access_token()
-    resp = google.get('userinfo', token=token)
-    user_info = resp.json()
-    user = config.oauth.google.userinfo()
-    if db.session.query(
-        db.exists().where(User.email == user['email'])
-    ).scalar():
-        session['profile'] = user_info
-        session.permanent = True
-        return 'Logged in', HTTPStatus.OK
-    return 'No such user', HTTPStatus.BAD_REQUEST
-
-
-def logout_oauth() -> None:
-    """Функция выхода из аккаунта"""
-    for key in list(session.keys()):
-        session.pop(key)
-
-
-def main_func() -> str:
-    """Функция для теста, будет убрана"""
-    try:
-        email = dict(session)['profile']['email']
-    except KeyError:
-        email = 'NOBODY!!!'
-    return f'Hello, you are logged in as {email}!'
--- src/config.py
+++ src/config.py
@@ -1,11 +1,9 @@
-from datetime import timedelta
-
 import connexion
-from authlib.integrations.flask_client import OAuth
+from werkzeug.security import generate_password_hash
 
 from db.db import db, init_db
+from db.db_models import BasicRoleEnum, User
 from settings import Settings
-
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
 
@@ -15,26 +13,23 @@ connex_app = connexion.App(__name__,
 app = connex_app.app
 
 app.secret_key = settings.secret_key
-app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = (f'postgresql://{settings.pg_user}:'
                                          f'{settings.pg_password}@'
                                          f'{settings.host}/{settings.pg_db}')
 
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=settings.client_id,
-    client_secret=settings.client_secret,
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
-    client_kwargs={'scope': 'openid email profile'},
-)
 
 init_db(app)
 app.app_context().push()
 db.create_all()
+
+user = User(
+            first_name='super', second_name='user',
+            login='superuser', email='superuser@gmail.com',
+            password=generate_password_hash('admin'),
+            role=BasicRoleEnum.superadmin
+        )
+try:
+    db.session.add(user)
+    db.session.commit()
+except:
+    pass
